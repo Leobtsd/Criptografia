5c6d349012287ab4d3a869553356c8078b64637b
Leonardo Batista <154553285+Leobtsd@users.noreply.github.com>
2024-04-04 06:25:59 -0300

    Add files via upload

---
+++ criptografia.php
@@ -0,0 +1,81 @@
+<?php 
+
+
+function criptografar($frase) {
+    // Substitui os espaços por #
+    $frase = str_replace(" ", "#", $frase);
+    // Divide a frase em partes de 3 
+    $frase = str_split($frase, 3);
+
+    // Loop pelas partes da frase
+   foreach ($frase as &$parte) {
+        // Gera uma letra aleatória de 'b' a 'e'
+        $letra_aleatoria = chr(rand(98, 101));
+        // Adiciona a letra aleatória ao final de cada parte da frase
+        $parte .= $letra_aleatoria;
+
+        // Verifica a última letra da parte da frase
+        $ultima_letra = substr($parte, -1);
+        switch ($ultima_letra) {
+            case 'b':
+                $parte .= chr(rand(97, 122));
+                break;
+            case 'c':
+                $parte .= chr(rand(97, 122)) . chr(rand(97, 122));
+                break;
+            case 'd':
+                $parte .= chr(rand(97, 122)) . chr(rand(97, 122)) . chr(rand(97, 122));
+                break;
+            case 'e':
+                $parte .= chr(rand(97, 122)) . chr(rand(97, 122)) . chr(rand(97, 122)) . chr(rand(97, 122));
+                break;
+           
+        }
+    }
+
+
+    $frase = implode("", $frase);
+    echo($frase);
+    
+    //var_dump($frase);
+}
+
+
+criptografar("ola meus amigos e amigas bom di");
+
+echo "<br>";
+
+function descriptografar($frase_criptografada) {
+    $frase_descriptografada = '';
+    $tamanho_frase = strlen($frase_criptografada);
+    $index = 0;
+
+    // Loop para percorrer a frase criptografada
+    while ($index < $tamanho_frase) {
+        // Obtém a parte da frase criptografada
+        $parte_criptografada = substr($frase_criptografada, $index, 4);
+
+        // Adiciona as três primeiras letras da parte à frase descriptografada
+        $frase_descriptografada .= substr($parte_criptografada, 0, 3);
+
+        // Determina quantas letras aleatórias foram adicionadas
+        $letra_aleatoria = substr($parte_criptografada, 3, 1);
+        $num_letras_a_remover = ord($letra_aleatoria) - ord('a');
+
+        // Avança o índice para a próxima parte da frase criptografada
+        $index += 4 + $num_letras_a_remover;
+    }
+
+    // Substitui os "#" por espaços
+    $frase_descriptografada = str_replace("#", " ", $frase_descriptografada);
+
+    // Retorna a frase descriptografada
+    echo $frase_descriptografada;
+}
+
+
+descriptografar("oladcxz#medtfwus#bjamieqecbgosdkhz#e#cehamibcgasdqrm#bobnm#ddjwdielghj")
+
+
+
+?>
