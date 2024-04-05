1f36e189d7bf592d3542998b6dad629a31bf9aca
Leonardo Batista <154553285+Leobtsd@users.noreply.github.com>
2024-04-05 08:16:04 -0300

    Update descriptografia.php

--- api/descriptografia.php
+++ api/descriptografia.php
@@ -1,5 +1,7 @@
 <?php
 
+header('Content-Type: text/html; charset=utf-8');
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $meuTexto = $_POST['meuTextoDescriptografar'];
    $descriptografada =  descriptografar($meuTexto);
@@ -9,19 +11,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 function descriptografar($frase_criptografada) {
     $frase_descriptografada = '';
-    $tamanho_frase = strlen($frase_criptografada);
+    $tamanho_frase = mb_strlen($frase_criptografada, 'UTF-8');
     $index = 0;
 
     // Loop para percorrer a frase criptografada
     while ($index < $tamanho_frase) {
         // Obtém a parte da frase criptografada
-        $parte_criptografada = substr($frase_criptografada, $index, 4);
+        $parte_criptografada = mb_substr($frase_criptografada, $index, 4, 'UTF-8');
 
         // Adiciona as três primeiras letras da parte à frase descriptografada
-        $frase_descriptografada .= substr($parte_criptografada, 0, 3);
+        $frase_descriptografada .= mb_substr($parte_criptografada, 0, 3, 'UTF-8');
 
         // Determina quantas letras aleatórias foram adicionadas
-        $letra_aleatoria = substr($parte_criptografada, 3, 1);
+        $letra_aleatoria = mb_substr($parte_criptografada, 3, 1, 'UTF-8');
         $num_letras_a_remover = ord($letra_aleatoria) - ord('a');
 
         // Avança o índice para a próxima parte da frase criptografada
@@ -32,8 +34,8 @@ function descriptografar($frase_criptografada) {
     $frase_descriptografada = str_replace("#", " ", $frase_descriptografada);
          
     return $frase_descriptografada;
-    
 }
+
 
 
 
