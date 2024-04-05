86db0ccc98eab49985bb8458763678da72905ed8
Leonardo Batista <154553285+Leobtsd@users.noreply.github.com>
2024-04-05 08:14:58 -0300

    Update criptografia.php

--- api/criptografia.php
+++ api/criptografia.php
@@ -1,25 +1,23 @@
 <?php 
 
 
+header('Content-Type: text/html; charset=utf-8');
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $meuTexto = $_POST['meuTextoCriptografar'];
     $criptografada = criptografar($meuTexto);
-
 }
-
-
-
 
 function criptografar($frase) {
     // Substitui os espaços por #
     $frase = str_replace(" ", "#", $frase);
     // Divide a frase em partes de 3 
-    $frase = str_split($frase, 3);
+    $frase = mb_str_split($frase, 3, 'UTF-8');
 
     // Loop pelas partes da frase
     foreach ($frase as &$parte) {
         // Verifica o tamanho da parte
-        $tamanho = strlen($parte);
+        $tamanho = mb_strlen($parte, 'UTF-8');
 
         if ($tamanho == 1) {
             $parte .= "##"; // Adiciona dois espaços vazios para completar 3 caracteres
@@ -33,7 +31,7 @@ function criptografar($frase) {
         $parte .= $letra_aleatoria;
 
         // Verifica a última letra da parte da frase
-        $ultima_letra = substr($parte, -1);
+        $ultima_letra = mb_substr($parte, -1, 1, 'UTF-8');
         switch ($ultima_letra) {
             case 'b':
                 $parte .= chr(rand(97, 122));
@@ -50,9 +48,9 @@ function criptografar($frase) {
         }
     }
 
-  
     $frase = implode("", $frase);
     return $frase;
+}
     
 }
 
